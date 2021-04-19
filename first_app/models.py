05cfc2ab739a2d4853e21c79867f6b2d2e4d2a6e
Fedenko Maksym <feden2906@gmail.com>
2021-04-19 19:46:40 +0300

    create model

--- first_app/models.py
+++ first_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+
+class OfficeModel(models.Model):
+    name = models.CharField(max_length=10)
+    city = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'offices'
