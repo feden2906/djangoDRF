90b3f4784255e8f921ccd6cd648576a8d067148d
Fedenko Maksym <feden2906@gmail.com>
2021-04-19 23:45:44 +0300

    response do not back

--- first_app/views.py
+++ first_app/views.py
@@ -1,19 +1,27 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import OfficeSerializer
+from .models import OfficeModel
 
 
 class MyApiView(APIView):
     def get(self, *args, **kwargs):
-        return Response('Hi from myApiView')
+        filt = self.request.query_params.get('city', None)
+        qs = OfficeModel.objects.all()
+        if filt:
+            print(filt)
+            qs = qs.filter()
+        serializer = OfficeSerializer(qs, many=True)
+        return Response(serializer.data)
 
     def post(self, *args, **kwargs):
         data = self.request.data
         serializer = OfficeSerializer(data=data)
 
         if not serializer.is_valid():
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         serializer.save()
-        return Response('created')
+        return Response('created', status=status.HTTP_201_CREATED)
