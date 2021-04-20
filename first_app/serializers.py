e6d6fd5e106df4f9d7881c32f5cbc2760d1d9441
Fedenko Maksym <feden2906@gmail.com>
2021-04-20 14:08:55 +0300

    finished lesson 1

--- first_app/serializers.py
+++ first_app/serializers.py
@@ -6,4 +6,4 @@ from .models import OfficeModel
 class OfficeSerializer(ModelSerializer):
     class Meta:
         model = OfficeModel
-        fields = '__all__'
+        fields = '__all__'                         # ('name', 'city') або []  - виводимо лише обраны поля
--- first_app/views.py
+++ first_app/views.py
@@ -11,17 +11,24 @@ class MyApiView(APIView):
         filt = self.request.query_params.get('city', None)
         qs = OfficeModel.objects.all()
         if filt:
-            print(filt)
-            qs = qs.filter()
+            qs = qs.filter(city=filt)
         serializer = OfficeSerializer(qs, many=True)
         return Response(serializer.data)
 
     def post(self, *args, **kwargs):
         data = self.request.data
         serializer = OfficeSerializer(data=data)
-
         if not serializer.is_valid():
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
         serializer.save()
         return Response('created', status=status.HTTP_201_CREATED)
+
+    def patch(self, *args, **kwargs):
+        key = self.request.query_params.get('id')
+        office = OfficeModel.objects.get(pk=key)
+        data = self.request.data
+        serializer = OfficeSerializer(office, data=data)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.save()
+        return Response('updated', status=status.HTTP_200_OK)
