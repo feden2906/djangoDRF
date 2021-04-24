11c9c35ca4fec59e987e0f99a41a0e65347ce26c
Fedenko Maksym <feden2906@gmail.com>
2021-04-24 22:33:17 +0300

    one to many

--- first_app/models.py
+++ first_app/models.py
@@ -15,7 +15,8 @@ class EmployeeModel(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField
     city = models.CharField(max_length=30)
-    office = models.OneToOneField(OfficeModel, on_delete=models.CASCADE)          # One to One    для розширення таблиці
+    # office = models.OneToOneField(OfficeModel, on_delete=models.CASCADE)                             # One to One    для розширення таблиці
+    offices = models.ForeignKey(OfficeModel, on_delete=models.CASCADE, related_name='employee')      # One to Many   related_name='employee'   дає можливість з однієї моделі звертатися в іншу (повязану з нею)
 
     class Meta:
         db_table = 'employees'
