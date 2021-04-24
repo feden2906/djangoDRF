c7a93a9c1d0a4cd71e8571e18f57b4ca21a92704
Fedenko Maksym <feden2906@gmail.com>
2021-04-24 22:42:24 +0300

    many to many

--- first_app/models.py
+++ first_app/models.py
@@ -15,8 +15,9 @@ class EmployeeModel(models.Model):
     name = models.CharField(max_length=30)
     age = models.IntegerField
     city = models.CharField(max_length=30)
-    # office = models.OneToOneField(OfficeModel, on_delete=models.CASCADE)                             # One to One    для розширення таблиці
-    offices = models.ForeignKey(OfficeModel, on_delete=models.CASCADE, related_name='employee')      # One to Many   related_name='employee'   дає можливість з однієї моделі звертатися в іншу (повязану з нею)
+    # office = models.OneToOneField(OfficeModel, on_delete=models.CASCADE)                                # One to One    для розширення таблиці
+    # offices = models.ForeignKey(OfficeModel, on_delete=models.CASCADE, related_name='employee')         # One to Many   related_name=''  дає можливість з однієї моделі звертатися в іншу (повязану з нею)
+    offices = models.ManyToManyField(OfficeModel, related_name="employee", db_column='office_employee')   # Many to Many  db_column=''  створить носу заблицю в якій буде зберігати залежності (id => id)
 
     class Meta:
         db_table = 'employees'
